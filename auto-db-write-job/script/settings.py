91f75db20d7535dc1655665fee1ddca62f30ff53
Inadati <drumake.unr@gmail.com>
2020-03-17 23:05:29 +0900

    dotenv comment out

--- auto-db-write-job/script/settings.py
+++ auto-db-write-job/script/settings.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
-load_dotenv("../../.env")
+# load_dotenv(verbose=True)
+# load_dotenv("../../.env")
 
 FILE_NAME = "covid19.xlsx"
 AWJ_DB_CONNECT_SETUP = os.environ.get("AWJ_DB_CONNECT_SETUP").strip("\"")
